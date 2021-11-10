c1c10d8adcbb12713accb4d2c1a311effcaddc6e
Sebastian Hurtado <hurtadosebastian037@gmail.com>
2021-11-10 20:05:33 -0300

    restaurante

--- desafio10/src/App.js
+++ desafio10/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
+function hacerPedido(a){
+  alert(a);
+}
+
 function App() {
+  //props
+  let cantMesas=10;
+  let liberado=true;
+  let mesasDispo=[]
+
+  for (let index = 1; index <= cantMesas; index++) {
+    mesasDispo.push(`Mesa Nº${index}`);
+  }
+  //render del componente
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="text-center">Lo de Pepe</h1>
+      <div className="row">
+        <div className="col-6">
+          {
+            mesasDispo.map((mesa,i)=>{
+              return(
+                <div
+                    id={i+1}
+                    className="text-center bg-success"
+                    onClick={()=>hacerPedido(i+1)}>
+                  <h3>{mesa}</h3>
+                </div>
+              );
+            })
+          }
+        </div>
+        <div className="col-6">
+
+        </div>
+        <div className="col-6">
+
+        </div>
+      </div>
     </div>
   );
 }
